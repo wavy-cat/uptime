99a4c0744713ab0eef042db58ffcf3ec5cd03aeb
WavyCat <52543076+wavy-cat@users.noreply.github.com>
2024-12-30 00:29:20 +0500

    Update uptime.config.ts

--- uptime.config.ts
+++ uptime.config.ts
@@ -26,7 +26,7 @@ const workerConfig = {
       // `method` should be a valid HTTP Method
       method: 'GET',
       // `target` is a valid URL
-      target: 'https://wavycat.ru',
+      target: 'https://wavycat.ru/weather',
       // [OPTIONAL] `tooltip` is ONLY used at status page to show a tooltip
       // tooltip: 'This is a tooltip for this monitor',
       // [OPTIONAL] `statusPageLink` is ONLY used for clickable link at status page
